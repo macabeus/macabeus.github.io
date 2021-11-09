ecfcf615b2267a74b41a6bad349ce2e4e7f5f138
macabeus <bruno.macabeus@gmail.com>
2021-11-09 00:47:03 +0000

    components: add Header

---
+++ src/components/Header.svelte
@@ -0,0 +1,43 @@
+<style>
+  .header {
+    border-bottom-style: solid;
+    border-bottom-width: 1px;
+    border-bottom-color: #cbd5e0;
+    padding-bottom: 1rem;
+  }
+
+  .logo {
+    height: 27px;
+  }
+
+  nav {
+    padding-top: 1rem;
+  }
+
+  a {
+    color: #5f6c80;
+    text-decoration: none;
+    transition: all 0.3s ease-in-out 0s;
+  }
+
+  a:hover {
+    color: #000;
+    text-decoration: underline;
+  }
+
+  a:not(:last-of-type) {
+    margin-right: 1rem;
+  }
+</style>
+
+<div class="header">
+  <img class="logo" src="../static/logo.svg" alt="Logo" >
+
+  <nav>
+    <a href="/">Home</a>
+    <a href="/essays">Essays</a>
+    <a href="/talks">Talks</a>
+    <a href="/projects">Projects</a>
+    <a href="/communities">Communities</a>
+  </nav>
+</div>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import Header from '../components/Header.svelte'
+</script>
+
+<style>
+  body {
+    max-width: 1024px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  main {
+    margin-top: 4rem;
+    margin-bottom: 4rem;
+  }
+
+  @media screen and (min-width: 640px) {
+    body {
+      padding: 0.5rem;
+    }
+  }
+</style>
+
+<svelte:head>
+  <title>Macabeus' Hub</title>
+</svelte:head>
+
+<body>
+  <Header />
+
+  <main>
+    <slot></slot>
+  </main>
+</body>
