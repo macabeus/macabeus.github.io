5573ec4cdcef57cfde97615aeebec50eb9f5c579
macabeus <bruno.macabeus@gmail.com>
2021-11-09 23:35:16 +0000

    content: add bio

---
+++ src/components/Avatar.svelte
@@ -0,0 +1,12 @@
+<style>
+  img {
+    width: 256px;
+    height: 256px;
+    border-radius: 50%;
+  }
+</style>
+
+<img
+  src="https://avatars.githubusercontent.com/u/9501115"
+  alt="Profile"
+>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import Avatar from '../components/Avatar.svelte';
+</script>
+
+<article>
+  <Avatar />
+
+  <h1>Hi, I'm Macabeus! 🙋🏻‍♂️</h1>
+
+  <p>
+    I'm a Sr. Full-Stack Engineer at OutSystems, building the runtime for a visual language that powers a
+    robust low-code platform. In other words, my work is to develop the libraries used when running
+    the application in the browser, as well as turning a visual language into JS.
+  </p>
+
+  <p>
+    I learned how to code using open sources projects, so I love helping them in different ways. I have
+    several of my personal projects on GitHub, approaching different topics, including reverse engineering,
+    compilers, and text editors.
+  </p>
+
+  <p>
+    Last but not least, I love developer communities, and I have been helping many of them for years now,
+    both as an organizer and speaker.
+  </p>
+
+  <p>
+    Got a project to discuss? Want me to speak at your event? Anything else? Let's chat! Feel free to
+    email me: <a href="mailto:bruno.macabeus@gmail.com">bruno.macabeus@gmail.com</a>.
+  </p>
+</article>
