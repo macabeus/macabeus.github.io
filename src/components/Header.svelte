5a4f6c8481a6df5aacd361871560a07388f990fb
macabeus <bruno.macabeus@gmail.com>
2021-11-25 23:59:38 +0000

    header: add social links

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { assets } from '$app/paths'
   import RouteLink from './RouteLink.svelte'
+  import SocialLinks from './SocialLinks.svelte'
 </script>
 
 <style>
@@ -15,8 +16,19 @@
     height: 27px;
   }
 
-  nav {
+  .links-container {
     padding-top: 1rem;
+
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+  }
+
+  @media screen and (min-width: 768px) {
+    .links-container {
+      align-items: center;
+      flex-direction: row;
+    }
   }
 
   :global(nav > *:not(:last-of-type)) {
@@ -27,11 +39,15 @@
 <div class="header">
   <img class="logo" src={`${assets}/logo.svg`} alt="Logo">
 
-  <nav>
-    <RouteLink path="/">Home</RouteLink>
-    <RouteLink path="/essays">Essays</RouteLink>
-    <RouteLink path="/talks">Talks</RouteLink>
-    <RouteLink path="/projects">Projects</RouteLink>
-    <RouteLink path="/communities">Communities</RouteLink>
-  </nav>
+  <div class="links-container">
+    <nav>
+      <RouteLink path="/">Home</RouteLink>
+      <RouteLink path="/essays">Essays</RouteLink>
+      <RouteLink path="/talks">Talks</RouteLink>
+      <RouteLink path="/projects">Projects</RouteLink>
+      <RouteLink path="/communities">Communities</RouteLink>
+    </nav>
+
+    <SocialLinks />
+  </div>
 </div>
---
+++ src/components/SocialLinks.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import Fa from 'svelte-fa/src/fa.svelte'
+  import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub'
+  import { faMedium } from '@fortawesome/free-brands-svg-icons/faMedium'
+  import { faLinkedin } from '@fortawesome/free-brands-svg-icons/faLinkedin'
+  import { faStackOverflow } from '@fortawesome/free-brands-svg-icons/faStackOverflow'
+  import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter'
+</script>
+
+<style>
+  a ~ a {
+    margin-left: 1rem;
+  }
+</style>
+
+<address>
+  <a href="https://github.com/macabeus" target="_blank" rel="noopener noreferrer">
+    <Fa icon={faGithub} />
+  </a>
+
+  <a href="https://medium.com/@macabeus" target="_blank" rel="noopener noreferrer">
+    <Fa icon={faMedium} />
+  </a>
+
+  <a href="https://linkedin.com/in/macabeus/" target="_blank" rel="noopener noreferrer">
+    <Fa icon={faLinkedin} />
+  </a>
+
+  <a href="https://stackoverflow.com/users/3440266/macabeus" target="_blank" rel="noopener noreferrer">
+    <Fa icon={faStackOverflow} />
+  </a>
+
+  <a href="https://twitter.com/bmacabeus" target="_blank" rel="noopener noreferrer">
+    <Fa icon={faTwitter} />
+  </a>
+</address>
