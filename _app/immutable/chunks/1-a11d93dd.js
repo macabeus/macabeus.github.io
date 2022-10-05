1ed1316b5231a609a070483f0ac858437a7743e0
macabeus <bruno.macabeus@gmail.com>
2022-10-05 22:58:04 +0100

    Updates

--- _app/immutable/chunks/1-a11d93dd.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-57aa697d.js";export{t as component};
---
+++ _app/immutable/chunks/1-d208aa43.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-ecd99739.js";export{t as component};
